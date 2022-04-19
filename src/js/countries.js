aa52730c52adbeb3f42384db0f179e612e40519c
TatianaHorbenko <3435640@gmail.com>
2022-04-19 10:40:52 +0300

    4

--- src/js/countries.js
+++ src/js/countries.js
@@ -4,6 +4,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
+
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
@@ -36,7 +37,7 @@ function onInputChange() {
   function markup(data) {
     const markupData = data
       .map(({ flags: { svg }, name: { official } }) => {
-        return `<li><img src="${svg}" alt="${official}" width="80" height="50"/>${official}</li>`;
+        return `<li><img src="${svg}" alt="${official}" width="80" height="40"/>${official}</li>`;
       })
       .join('');
 
