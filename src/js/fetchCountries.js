73d256e952e4c82afcab6566a16efd87a376038c
TatianaHorbenko <3435640@gmail.com>
2022-04-20 21:51:04 +0300

    18

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,11 @@
 export default function fetchCountries(name) {
-    const address = 'https://restcountries.com/v3.1/name/';
-    const options = `?fields=name,capital,population,flags,languages`;
-  
-    return fetch(`${address}${name}${options}`).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-  };
+  const BASE_URL = 'https://restcountries.com/v3.1/name';
+  const OTHER_URL = `?fields=name,capital,population,flags,languages`;
+  return fetch(`${BASE_URL}/${name}${OTHER_URL}`)
+          .then(response => {
+              if (!response.ok) {
+                  throw error(response.status);
+              }
+          return response.json();
+      })
+};
